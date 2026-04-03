c693cdc740e6729baf01235b56b88f2de8fe711f
Tolomas3721 <thomasbeaulieu3721@gmail.com>
2026-04-02 22:52:38 -0400

    added cells

---
+++ src/engine/automata/cell.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <cstdint>
+
+#include "cell_info.h"
+
+
+
+class Cell {
+public:
+    Cell(){
+        data = 0;
+    }
+
+    Cell(CellInfo::MainType main_type, CellInfo::SubType subtype, std::uint32_t color_id){
+        data = 0;
+        data |= (main_type & MASKS::MAIN_TYPE) << SHIFTS::MAIN_TYPE;
+        data |= (subtype & MASKS::SUBTYPE) << SHIFTS::SUBTYPE;
+        data |= (color_id & MASKS::COLOR) << SHIFTS::COLOR;
+    }
+
+    // returns the color id for the specific type, use get_full_color_id for the 'true' index
+    inline std::uint32_t get_color_id(){
+        return (data >> SHIFTS::COLOR) & MASKS::COLOR;
+    }
+
+    inline std::uint32_t get_full_color_id(){
+        // this assumes that color, subtype and main type are all on the lowest bits and contiguous
+        constexpr std::uint32_t mask = ((MASKS::COLOR << SHIFTS::COLOR) | 
+                                        (MASKS::SUBTYPE << SHIFTS::SUBTYPE) | 
+                                        (MASKS::MAIN_TYPE << SHIFTS::MAIN_TYPE));
+        return data & mask;
+    }
+
+    inline std::uint32_t get_main_type(){
+        return (data >> SHIFTS::MAIN_TYPE) & MASKS::MAIN_TYPE;
+    }
+
+    inline std::uint32_t get_subtype(){
+        return (data >> SHIFTS::SUBTYPE) & MASKS::SUBTYPE;
+    }
+
+    inline std::uint32_t get_full_type(){
+        // assumes types are contiguous and in the lowest bits
+        constexpr std::uint32_t mask = ((MASKS::MAIN_TYPE << SHIFTS::MAIN_TYPE) |
+                                        (MASKS::SUBTYPE << SHIFTS::SUBTYPE));
+        return data & mask;
+    }
+
+private:
+    // this must be 32 bits to comply with the shaders
+    std::uint32_t data;
+
+    // these are hard-coded, i technically could make them auto generate, but it would be unreadable
+    enum MASKS : std::uint32_t {
+        // 2 bits
+        MAIN_TYPE = 3,
+        // 1024 subtypes for 4096 total types... wont be too small, hopefully lol
+        SUBTYPE = 0x3FF,
+        // 4 bits for color, 16 colors per type
+        // 4 bytes * 4096 types * 16 colors = 256kb of palette
+        COLOR = 0xF
+        /* 
+         * 16 free bits for now, can store 
+         * 1 65536 values or 
+         * 2 256 values or 
+         * 4 16 values or 
+         * 8 4 values or
+         * 16 flags
+         * more types?
+         * heat?
+         * electricity?
+         * on fire?
+         * lifetime?
+         * velocity?wdwdasdwad
+         * light (tint, light emission, etc)
+         * stains? (color override)
+        */
+    };
+
+    enum SHIFTS : std::uint32_t {
+        MAIN_TYPE = 0,
+        SUBTYPE = 2,
+        COLOR = 12
+    };
+};
---
+++ src/engine/automata/cell_info.h
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+
+
+// stores data shared between all cells of the same type
+struct CellType {
+public:
+    std::uint32_t type;
+    float density;
+    float flammability;
+    float conductivity;
+    std::string name;
+
+    CellType(){
+        type = DEFAULT_TYPE;
+        density = DEFAULT_DENSITY;
+        flammability = DEFAULT_FLAMMABILITY;
+        conductivity = DEFAULT_CONDUCTIVITY;
+        name = DEFAULT_NAME;
+    }
+
+private:
+    static constexpr float DEFAULT_DENSITY = 1.0;
+    static constexpr float DEFAULT_FLAMMABILITY = 0.0;
+    static constexpr float DEFAULT_CONDUCTIVITY = 0.0;
+    // compiler wont allow std::string here, works the same anyway
+    static constexpr const char* DEFAULT_NAME = "Bagel bag";
+    // could be any, doesnt matter
+    // if we still have this, then we have an issue anyway lol
+    static constexpr std::uint32_t DEFAULT_TYPE = 0;
+};
+
+
+
+namespace CellInfo {
+
+    std::vector<CellType> load_cell_types(std::string path){
+        if(!path.ends_with(".csv")){
+            // LOG
+            // wrong format, though we could still try to parse it
+            return {};
+        }
+
+        std::ifstream file(path);
+        if(!file.is_open()){
+            // LOG("could not open file :(")
+            return {};
+        }
+
+        // parse the headers
+        std::string line;
+        std::getline(file, line);
+        std::stringstream line_stream(std::move(line));
+        std::string field;
+        std::vector<std::string> headers;
+
+        while(std::getline(line_stream, field, ',')) {
+            headers.push_back(field);
+        }
+
+        // parse the csv file
+        std::vector<std::string> row(headers.size());
+        std::vector<CellType> cell_types;
+
+        while(std::getline(file, line)){
+            line_stream.str(std::move(line));
+            std::unordered_map<std::string, std::string> mappings;
+            // store default values
+            CellType cell = {};
+
+            auto current_header = headers.begin();
+            while(std::getline(line_stream, field, ',') && current_header < headers.end()){
+                mappings.insert(std::make_pair(*current_header, field));
+                current_header++;
+            }
+
+            auto it = mappings.find("density");
+            if(it != mappings.end()) cell.density = std::atof(it->second.c_str());
+            
+            it = mappings.find("flammability");
+            if(it != mappings.end()) cell.flammability = std::atof(it->second.c_str());
+            
+            it = mappings.find("conductivity");
+            if(it != mappings.end()) cell.conductivity = std::atof(it->second.c_str());
+            
+            it = mappings.find("name");
+            if(it != mappings.end()) cell.name = it->second;
+
+            it = mappings.find("type");
+            if(it != mappings.end()) cell.type = std::atoi(it->second.c_str());
+
+            cell_types.push_back(cell);
+        }
+        
+        file.close();
+        // make sure their id is in order
+        std::sort(cell_types.begin(), cell_types.end(), [](CellType a, CellType b){
+            return a.type < b.type;
+        });
+        for(int i = 1; i < cell_types.size(); i++){
+            if(cell_types[i].type == cell_types[i - 1].type){
+                // type cant be doubled, we report the error
+                // LOG("error, two cells have the same type, give the cells, la bla bla")
+                cell_types.erase(cell_types.begin() + i);
+                i--;
+            }
+        }
+        return cell_types;
+    }
+
+    enum MainType : std::uint32_t {
+        GAS = 0,
+        LIQUID = 1,
+        POWDER = 2,
+        SOLID = 3
+    };
+
+    // here for some subtypes, but most are defined at runtime
+    // these go per main type, so multiple can have same value as long as they are not the same main type
+    enum SubType {
+        AIR = 0,
+        WATER = 0,
+        SAND = 0,
+        ROCK = 0
+    };
+}
