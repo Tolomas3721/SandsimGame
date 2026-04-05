880d0d1d13bcdf30f2284d371d3fb4b4a5b6b112
Tolomas3721 <thomasbeaulieu3721@gmail.com>
2026-04-04 21:46:56 -0400

    simulation map

--- src/engine/automata/chunk.h
+++ src/engine/automata/chunk.h
@@ -14,19 +14,19 @@
 class Chunk {
 public:
     // must all be powers of 2
-    static constexpr unsigned int CELL_X = 256;
-    static_assert(std::has_single_bit(CELL_X), "CELL_X in Chunk class is not power of 2");
-    static constexpr unsigned int CELL_Y = 256;
-    static_assert(std::has_single_bit(CELL_X), "CELL_Y in Chunk class is not power of 2");
+    static constexpr size_t CHUNK_SIZE_X = 256;
+    static_assert(std::has_single_bit(CHUNK_SIZE_X), "CHUNK_SIZE_X in Chunk class is not power of 2");
+    static constexpr size_t CHUNK_SIZE_Y = 256;
+    static_assert(std::has_single_bit(CHUNK_SIZE_Y), "CHUNK_SIZE_Y in Chunk class is not power of 2");
 
-    static constexpr unsigned int SUBCHUNK_X = 8;
-    static_assert(CELL_X % SUBCHUNK_X == 0 && SUBCHUNK_X <= CELL_X, "SUBCHUNK_X in Chunk class has an invalid value");
-    static constexpr unsigned int SUBCHUNK_Y = 8;
-    static_assert(CELL_Y % SUBCHUNK_Y == 0 && SUBCHUNK_Y <= CELL_Y, "SUBCHUNK_Y in Chunk class has an invalid value");
+    static constexpr size_t SUBCHUNK_SIZE_X = 8;
+    static_assert(CHUNK_SIZE_X % SUBCHUNK_SIZE_X == 0 && SUBCHUNK_SIZE_X <= CHUNK_SIZE_X, "SUBCHUNK_X in Chunk class has an invalid value");
+    static constexpr size_t SUBCHUNK_SIZE_Y = 8;
+    static_assert(CHUNK_SIZE_Y % SUBCHUNK_SIZE_Y == 0 && SUBCHUNK_SIZE_Y <= CHUNK_SIZE_Y, "SUBCHUNK_Y in Chunk class has an invalid value");
 
     
-    inline Cell& get_cell(unsigned int x, unsigned int y){
-        if(x >= CELL_X || y >= CELL_X) throw std::out_of_range("cell position is out of range");
+    inline Cell& get_cell(size_t x, size_t y){
+        if(x >= CHUNK_SIZE_X || y >= CHUNK_SIZE_X) throw std::out_of_range("cell position is out of range");
         return cells[y][x];
     }
 
@@ -35,12 +35,14 @@ public:
         return cells[y][x];
     }
 
-    inline std::mdspan<Cell, std::extents<size_t, SUBCHUNK_X, SUBCHUNK_Y>> get_subchunk(unsigned int x, unsigned int y){
-        return std::mdspan<Cell, std::extents<size_t, SUBCHUNK_X, SUBCHUNK_Y>>(&cells[y * SUBCHUNK_Y][x * SUBCHUNK_X]);
+    inline std::mdspan<Cell, std::extents<size_t, SUBCHUNK_SIZE_X, SUBCHUNK_SIZE_Y>> get_subchunk(size_t x, size_t y){
+        return std::mdspan<Cell, std::extents<size_t, SUBCHUNK_SIZE_X, SUBCHUNK_SIZE_Y>>(
+            &cells[y * SUBCHUNK_SIZE_Y][x * SUBCHUNK_SIZE_X]
+        );
     }
 
 private:
     // use X as the minor for better cache access
     // but its all 2x2 anyway,unless maybe liquids will move sideways more
-    std::array<std::array<Cell, CELL_X>, CELL_Y> cells;
+    std::array<std::array<Cell, CHUNK_SIZE_X>, CHUNK_SIZE_Y> cells;
 };
---
+++ src/engine/automata/simulation_map.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <bit>
+#include <stdexcept>
+
+#include "cell.h"
+#include "chunk.h"
+
+
+
+class SimulationMap{
+public:
+
+    inline Cell& get_cell(size_t x, size_t y){
+        // error handling is done in there, buddy
+        return get_cell(x / Chunk::CHUNK_SIZE_X, y / Chunk::CHUNK_SIZE_Y, x % Chunk::CHUNK_SIZE_X, y % Chunk::CHUNK_SIZE_Y);
+    }
+
+    inline Cell& get_cell(size_t chunk_x, size_t chunk_y, size_t x, size_t y){
+        if(chunk_x >= MAP_SIZE_X || chunk_y >= MAP_SIZE_Y) throw std::out_of_range("chunk position is out of range");
+        return chunks[chunk_x][chunk_y].get_cell(x, y);
+    }
+
+    inline Cell& operator[](size_t chunk_x, size_t chunk_y, size_t x, size_t y){
+        return chunks[chunk_x][chunk_y][x, y];
+    }
+
+    static constexpr size_t MAP_SIZE_X = 8;
+    static_assert(std::has_single_bit(MAP_SIZE_X), "MAP_SIZE_X in SimulationMap class is not power of 2");
+    static constexpr size_t MAP_SIZE_Y = 8;
+    static_assert(std::has_single_bit(MAP_SIZE_Y), "MAP_SIZE_Y in SimulationMap class is not power of 2");
+
+private:
+    std::array<std::array<Chunk, MAP_SIZE_X>, MAP_SIZE_Y> chunks;
+};
